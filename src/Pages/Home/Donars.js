579267a00f6f148e859a01eb8710c6beccc8d869
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-03-06 14:18:26 +0600

    new changes

--- src/Pages/Home/Donars.js
+++ src/Pages/Home/Donars.js
@@ -37,20 +37,18 @@ const Donars = () => {
                     </div>
                 )}
                 <div>
-                    {
-                        donars.length > 0 ? <h1 className='text-3xl font-bold tracking-wider text-center mt-4'>No Donars Found</h1> : <><div class="mx-auto mt-24 grid w-10/12 grid-cols-1 md:grid-cols-3 gap-10 ">
-                            {donars?.slice(0, 6).map(item =>
-                                <DonarCard item={item}></DonarCard>
-                            )}
-                        </div>
-                            <div className='flex justify-center mt-16'>
-                                <a href="#_" class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-primary rounded-lg group">
-                                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#502b7c] rounded-full group-hover:w-56 group-hover:h-56"></span>
-                                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-                                    <span class="relative"><Link to="/donars">See All Donors</Link></span>
-                                </a>
-                            </div></>
-                    }
+                    <div class="mx-auto mt-24 grid w-10/12 grid-cols-1 md:grid-cols-3 gap-10 ">
+                        {donars?.slice(0, 6).map(item =>
+                            <DonarCard item={item}></DonarCard>
+                        )}
+                    </div>
+                    <div className='flex justify-center mt-16'>
+                        <a href="#_" class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-primary rounded-lg group">
+                            <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#502b7c] rounded-full group-hover:w-56 group-hover:h-56"></span>
+                            <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                            <span class="relative"><Link to="/donars">See All Donors</Link></span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div >
